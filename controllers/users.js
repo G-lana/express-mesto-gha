9bc3e7ae48abf8b3cff4a70aa8c4200ea652a7e1
Svetlana Galkina <G.lana08@yandex.ru>
2023-03-11 17:19:16 +0300

    Проектная работа 13

--- controllers/users.js
+++ controllers/users.js
@@ -7,10 +7,16 @@ module.exports.getUsers = (req, res) => {
 };
 module.exports.getCurrentUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (user === null) {
+        res.status(404).send({ message: "Запрашиваемый ресурс не найден" });
+        return;
+      }
+      res.send({ data: user });
+    })
     .catch((err) => {
       if (err.name === "CastError") {
-        res.status(404).send({
+        res.status(400).send({
           message: `Не удалось найти пользователя с id - ${req.params.userId}`,
         });
       }
